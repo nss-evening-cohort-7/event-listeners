f2821cabf11cef671bcbeef66c13e8cda4c445cf
Callan Morrison <callan.l.morrison@gmail.com>
2018-03-20 20:46:49 -0500

    setup

---
+++ main.js
@@ -0,0 +1,56 @@
+const students = [
+    {
+        avatar: "https://robohash.org/inciduntsapientetenetur.png?size=100x100&set=set1",
+        firstName: "Marinna",
+        lastName: "Olliver",
+        catchPhrase: "Optional global adapter"
+    },
+    {
+        avatar: "https://robohash.org/facereinventoreexpedita.png?size=100x100&set=set1",
+        firstName: "Shalne",
+        lastName: "Roddell",
+        catchPhrase: "Vision-oriented optimizing installation"
+    },
+    {
+        avatar: "https://robohash.org/sintquasnam.png?size=100x100&set=set1",
+        firstName: "Kamila",
+        lastName: "Girardey",
+        catchPhrase: "Ergonomic human-resource toolset"
+    },
+    {
+        avatar: "https://robohash.org/autautfugiat.png?size=100x100&set=set1",
+        firstName: "Conn",
+        lastName: "Bemment",
+        catchPhrase: "Mandatory 4th generation model"
+    },
+    {
+        avatar: "https://robohash.org/inciduntameteum.png?size=100x100&set=set1",
+        firstName: "Jeanelle",
+        lastName: "Inglesent",
+        catchPhrase: "Configurable value-added architecture"
+    },
+    {
+        avatar: "https://robohash.org/vitaequiafugiat.png?size=100x100&set=set1",
+        firstName: "Xymenes",
+        lastName: "Nickols",
+        catchPhrase: "Virtual coherent standardization"
+    },
+    {
+        avatar: "https://robohash.org/oditexassumenda.png?size=100x100&set=set1",
+        firstName: "Mandel",
+        lastName: "Haycox",
+        catchPhrase: "Balanced zero tolerance throughput"
+    },
+    {
+        avatar: "https://robohash.org/ipsamnamiusto.png?size=100x100&set=set1",
+        firstName: "Salim",
+        lastName: "Brunker",
+        catchPhrase: "Synergized impactful middleware"
+    },
+    {
+        avatar: "https://robohash.org/quiaofficiaullam.png?size=100x100&set=set1",
+        firstName: "Melanie",
+        lastName: "Ivain",
+        catchPhrase: "Multi-channelled solution-oriented artificial intelligence"
+    }
+];
